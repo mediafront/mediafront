49a8e95ebfd6b929bbe3d0da85e280921cf7b6df
Travis Tidwell <travist349@gmail.com>
2010-07-02 02:04:05 +0000

    Fixed an issue with the Flash player where you can now have images in the embedded player.

--- players/osmplayer/player/flash/skins/default/mediaPlayer.as
+++ players/osmplayer/player/flash/skins/default/mediaPlayer.as
@@ -8,4 +8,7 @@ function onResize( deltaX:Number, deltaY:Number ) : void
 {
 	backgroundMC.width = backgroundMC.width + deltaX;
 	backgroundMC.height = backgroundMC.height + deltaY;
+	
+	// Now resize the preview.
+	preview.resize( backgroundMC.getRect(this) );
 }
--- players/osmplayer/player/js/source/jquery.media.flash.js
+++ players/osmplayer/player/js/source/jquery.media.flash.js
@@ -50,6 +50,7 @@
          var _this = this;
          this.player = null;
          this.videoFile = null;
+         this.preview = '';
          this.ready = false;
          
          // Translate the messages.
@@ -63,8 +64,9 @@
             "mediaMeta":"meta"        
          };
          
-         this.createMedia = function( videoFile ) {
+         this.createMedia = function( videoFile, preview ) {
             this.videoFile = videoFile;
+            this.preview = preview;
             this.ready = false;
             var playerId = (settings.id + "_media");            
             var rand = Math.floor(Math.random() * 1000000); 
@@ -176,6 +178,7 @@
                config:"config",
                id:"mediafront_player",
                file:this.videoFile.path,
+               image:this.preview,
                skin:settings.skin
             };
             if( this.videoFile.stream ) {
