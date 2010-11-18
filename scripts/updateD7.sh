74107a82bc8766a97089fa04db265a2811c63e09
Travis Tidwell <travist349@gmail.com>
2010-11-18 22:47:40 +0000

    Don't want to place these in the scripts folder.

--- scripts/updateD7.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-DIR="$( cd "$( dirname "$0" )" && pwd )"
-rsync -av --exclude ".svn/" --exclude ".git/" --exclude "CVS/" $DIR/../players/osmplayer/player $DIR/../../../../../../drupal7/sites/all/modules/mediafront/players/osmplayer
